f626ba67b224476754f14fc654d8a1999b0afd17
Sarsande-Sunil <93374701+Sarsande-Sunil@users.noreply.github.com>
2022-04-23 14:45:34 +0530

    main2

--- flowerbackend/src/models/flowermodel.js
+++ flowerbackend/src/models/flowermodel.js
@@ -21,8 +21,8 @@ const potSchemas = new mongoose.Schema(
   {
     id: { type: Number, required: true },
     image: { type: String, required: true },
-    name: { type: String, require: true, minlength: 4, maxlength: 12 },
-    price: { type: Number, require: true, minlength: 2, maxlength: 6 },
+    name: { type: String, require: true },
+    price: { type: Number, require: true },
   },
   {
     versionKey: false,
